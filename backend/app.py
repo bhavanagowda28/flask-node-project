0c8097025618d913957bcdf471a7ce3d3e1cdce4
Bhavana S Gowda <bhavanagowda28@gmail.com>
2026-05-20 18:24:40 +0530

    Final submission update

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-
 CORS(app)
 
-@app.route('/api/data', methods=['POST'])
-def receive_data():
-
-    data = request.json
-
-    name = data.get('name')
-    email = data.get('email')
-
-    print("Received Data:")
-    print(name)
-    print(email)
-
+@app.route('/api/data')
+def get_data():
     return jsonify({
-        "message": "Data received successfully"
+        "message": "Backend working successfully 🚀"
     })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
-
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
 
 
